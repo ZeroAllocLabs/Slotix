b591b55014dedae09b88c427e6a9271b98104742
Saran-006 <srisaranp123@gmail.com>
2026-01-20 22:11:53 +0530

    started api package

---
+++ src/main/java/org/example/api/getSample.java
@@ -0,0 +1,26 @@
+package org.example.api;
+import okhttp3.*;
+
+public class getSample {
+
+    void makeCall(){
+
+
+    OkHttpClient client = new OkHttpClient();
+
+    String url = "http://127.0.0.1:7000/static/sample1.png";
+
+    Request request = new Request.Builder().get().url(url).build();
+
+    try(Response resp=client.newCall(request).execute()){
+        if (resp.body() != null) {
+        byte[] bytes = resp.body().bytes();
+        System.out.println(new String(bytes));
+        }
+    }
+    catch (Exception e){
+        System.out.println("error fetching bytes form server. may be server offline");
+    }
+    }
+
+}
